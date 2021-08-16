b37be508ba17daa9ffe23affc2f1f95ecb6c4e84
Adithya09 <ap2g20@soton.ac.uk>
2021-08-16 16:06:02 +0100

    Ray casting algorithm

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ let drawingManager;
 //Defining a global coordinates variable
 let coordinates
 
+//Deefining a global array of polygons
+let polygonArray = [];
+
 //Function to make an id for each polygonOptions//Function to generate a random code for each polygon
 function makeID(){
   var result           = '';
@@ -29,9 +32,6 @@ function initMap(){
     center: {lat: 51.507, lng: 0.127 }
   });
 
-
-
-
   //Creating a drawing manager tool
   drawingManager = new google.maps.drawing.DrawingManager({
   drawingMode: google.maps.drawing.OverlayType.MARKER,
@@ -50,51 +50,68 @@ function initMap(){
 });
   drawingManager.setMap(map);
 
- const geocoder = new google.maps.Geocoder();
- const infowindow = new google.maps.InfoWindow();
- document.getElementById("submit").addEventListener("click", () => {
-   reverseGeocode(geocoder, map, infowindow);
- });
- google.maps.event.addListener(drawingManager, "overlaycomplete", function(e){
-  var coordinatesArray = e.overlay.getPath().getArray();
-  coordinatesArray = makeID();
-  console.log(coordinatesArray);
-   const input = document.getElementById("latlng").value;
-   const latlngStr = input.split(",", 2);
-   var lat = parseFloat(latlngStr[0]);
-   var lng = parseFloat(latlngStr[1])
-   var curPosition = new google.maps.LatLng(lat, lng);
+  google.maps.event.addListener(drawingManager, "overlaycomplete", function(e){
+    var id = e.overlay.getPath().getArray();
+    var path =  e.overlay.getPath().getArray();
+    id = makeID();
+    console.log(id);
+    var polygon = e.overlay
 
-   if(google.maps.geometry.poly.containsLocation(curPosition, e.overlay)){
-    alert("These coordinates lie in the polygon: " + coordinatesArray);
-  }else{
-    alert("These coordinates do not lie in a polygon");
-  }
-});
+    google.maps.Polygon.prototype.Contains = function(point) {
+      var crossings = 0,
+      path = this.getPath();
 
- }
+      // for each edge
+      for (var i=0; i < path.getLength(); i++) {
+        var a = path.getAt(i),
+        j = i + 1;
+        if (j >= path.getLength()) {
+          j = 0;
+        }
+        var b = path.getAt(j);
+        if (rayCrossesSegment(point, a, b)) {
+          crossings++;
+        }
+      }
 
-function reverseGeocode(geocoder, map, infowindow){
-const input = document.getElementById("latlng").value;
- const latlngStr = input.split(",", 2);
- const latlng = {
-   lat: parseFloat(latlngStr[0]),
-   lng: parseFloat(latlngStr[1]),
- };
- geocoder
-   .geocode({ location: latlng })
-   .then((response) => {
-     if (response.results[0]) {
-       map.setZoom(11);
-       marker = new google.maps.Marker({
-         position: latlng,
-         map: map,
-       });
-       infowindow.setContent(response.results[0].formatted_address);
-       infowindow.open(map, marker);
-    } else {
-       window.alert("No results found");
+      // odd number of crossings?
+      return (crossings % 2 == 1);
+
+      function rayCrossesSegment(point, a, b) {
+        var px = point.lng(),
+        py = point.lat(),
+        ax = a.lng(),
+        ay = a.lat(),
+        bx = b.lng(),
+        by = b.lat();
+        if (ay > by) {
+          ax = b.lng();
+          ay = b.lat();
+          bx = a.lng();
+          by = a.lat();
+        }
+        // alter longitude to cater for 180 degree crossings
+        if (px < 0) { px += 360 };
+        if (ax < 0) { ax += 360 };
+        if (bx < 0) { bx += 360 };
+        if (py == ay || py == by) py += 0.00000001;
+        if ((py > by || py < ay) || (px > Math.max(ax, bx))) return false;
+        if (px < Math.min(ax, bx)) return true;
+        var red = (ax != bx) ? ((by - ay) / (bx - ax)) : Infinity;
+        var blue = (ax != px) ? ((py - ay) / (px - ax)) : Infinity;
+        return (blue >= red);
+      }
+    };
+
+    // const input = document.getElementById("latlng").value;
+    // const latlngStr = input.split(",", 2);
+    // var lat = parseFloat(latlngStr[0]);
+    // var lng = parseFloat(latlngStr[1])
+    // var point = new google.maps.LatLng(lat, lng);
+    var point = new google.maps.LatLng(52.05249047600099, -0.6097412109375);
+    var polygon = new google.maps.Polygon({path:[path]});
+    if (polygon.Contains(point)) {
+      // point is inside polygon
     }
-  })
-   .catch((e) => window.alert("Geocoder failed due to: " + e));
+  });
 }
