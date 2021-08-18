2f8511313f44092cb2c2da636c2fcc9acf8a8aef
Adithya09 <ap2g20@soton.ac.uk>
2021-08-18 18:53:22 +0100

    All internship requirement functionalities added

--- index.js
+++ index.js
@@ -4,31 +4,13 @@ let map;
 //Defining a global drawing manager Object
 let drawingManager;
 
-//Defining a global polygon ids variable
-let id;
-
 //Defining a global polygon array
 let polArray = [];
 
 /*
-Function to make an id for each polygonOptions//Function to generate a random
-code for each polygon
-*/
-function makeID(){
-  var result = '';
-  var characters =
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for ( var i = 0; i < 5; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() *
-    charactersLength));
-  }
-  return result;
-}
-
-/*
 Function to check if the given point lies within any of the polygons in the
-array
+array. Returns true if it lies inside and false if not to be used in a switch
+statement when the PIP check is triggered.
 */
 function checkPIP(){
   var isWithin = false;
@@ -46,6 +28,129 @@ function checkPIP(){
   }
   return isWithin;
 }
+
+/*
+Function to calculate distance and duration between points manually entered
+by user on the map
+*/
+function calcEnteredRoutes(){
+  //Getting the coordinates of point A and placing a marker at these coords
+  const marker1 = document.getElementById("marker1").value;
+  const latlngStr1 = marker1.split(",", 2);
+  var lat1 = parseFloat(latlngStr1[0]);
+  var lng1 = parseFloat(latlngStr1[1])
+  var marker1position = new google.maps.LatLng(lat1, lng1);
+  var mk1 = new google.maps.Marker({position: marker1position, map: map});
+
+  //Getting the coordinates of point B and placing a marker at these coords
+  const marker2 = document.getElementById("marker2").value;
+  const latlngStr2 = marker2.split(",", 2);
+  var lat2 = parseFloat(latlngStr2[0]);
+  var lng2 = parseFloat(latlngStr2[1]);
+  var marker2position = new google.maps.LatLng(lat2, lng2);
+  var mk2 = new google.maps.Marker({position: marker2position, map: map});
+
+  /*
+  Utilising the google maps directions API to calculate the distance from
+  marker A to marker B
+  */
+  let directionsService = new google.maps.DirectionsService();
+  let directionsRenderer = new google.maps.DirectionsRenderer();
+
+  //Existing map object displays directions
+  directionsRenderer.setMap(map);
+
+  // Create route from existing points used for markers
+  const route = {
+    origin: marker1position,
+    destination: marker2position,
+    travelMode: 'DRIVING',
+    unitSystem: google.maps.UnitSystem.IMPERIAL
+
+  }
+
+  //Anonymous function to capture directions
+  directionsService.route(route,
+    function(response, status) {
+      if (status !== 'OK') {
+        window.alert('Directions request failed due to ' + status);
+        return;
+      } else {
+        // Add route to the map
+        directionsRenderer.setDirections(response);
+
+        // Get data about the mapped routes
+        var directionsData = response.routes[0].legs[0];
+        if (!directionsData) {
+          window.alert('Directions request failed');
+          return;
+      }else {
+        document.getElementById('msg').innerHTML =
+        "Driving distance and duration from "
+        + marker1position + " to " + marker2position + " is "
+        + directionsData.distance.text + " (" + directionsData.duration.text
+        + ").";
+      }
+    }
+  });
+}
+
+ /*
+ Calculate routes based on two points clicked on the map by the user
+*/
+function calcClickedRoutes(){
+  google.maps.event.addListener(map, "click", function(event){
+    var clickedPoint1 = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
+    var click1marker = new google.maps.Marker({position: clickedPoint1, map: map});
+    document.getElementById("marker1").value = clickedPoint1;
+
+    google.maps.event.addListener(map, "click", function(event2){
+      var clickedPoint2 = new google.maps.LatLng(event2.latLng.lat(), event2.latLng.lng());
+      var click2marker = new google.maps.Marker({position: clickedPoint2, map: map});
+      document.getElementById("marker2").value = clickedPoint2;
+
+      //Calculating distance and duration
+      let directionsService = new google.maps.DirectionsService();
+      let directionsRenderer = new google.maps.DirectionsRenderer();
+
+      //Existing map object displays directions
+      directionsRenderer.setMap(map);
+
+      // Create route from existing points used for markers
+      const route = {
+        origin: clickedPoint1,
+        destination: clickedPoint2,
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+      }
+
+      //Anonymous function to capture directions
+      directionsService.route(route,
+        function(response, status) {
+          if (status !== 'OK') {
+            window.alert('Directions request failed due to ' + status);
+            return;
+          }else {
+            // Add route to the map
+            directionsRenderer.setDirections(response);
+
+            // Get data about the mapped routes
+            var directionsData = response.routes[0].legs[0];
+            if (!directionsData) {
+              window.alert('Directions request failed');
+              return;
+            }else {
+              document.getElementById('msg').innerHTML =
+              "Driving distance and duration from "
+              + clickedPoint1 + " to " + clickedPoint2 + " is "
+              + directionsData.distance.text + " (" + directionsData.duration.text
+              + ").";
+            }
+          }
+        });
+      });
+    });
+  }
 
 //Function to refresh the map
 function refresh(){
@@ -89,13 +194,8 @@ function initMap(){
   });
   drawingManager.setMap(map);
 
-  /*
-  Generating a unique ID for a polygon upon creation and adding it to the
-  polygon array
-  */
   google.maps.event.addListener(drawingManager, "overlaycomplete", function(e){
     if(e.type == google.maps.drawing.OverlayType.POLYGON){
-      var path =  e.overlay.getPath().getArray();
       polArray.push(e.overlay);
     }
   });
@@ -124,128 +224,20 @@ function initMap(){
       }
     });
 
-  /*
-  Calculating the distance between two points on the Map
-  */
+  //Calculate routes based on user input points
   document.getElementById("routeSubmit").addEventListener("click", () => {
-    //Getting the coordinates of point A and placing a marker at these coords
-    const marker1 = document.getElementById("marker1").value;
-    const latlngStr1 = marker1.split(",", 2);
-    var lat1 = parseFloat(latlngStr1[0]);
-    var lng1 = parseFloat(latlngStr1[1])
-    var marker1position = new google.maps.LatLng(lat1, lng1);
-    var mk1 = new google.maps.Marker({position: marker1position, map: map});
-
-    //Getting the coordinates of point B and placing a marker at these coords
-    const marker2 = document.getElementById("marker2").value;
-    const latlngStr2 = marker2.split(",", 2);
-    var lat2 = parseFloat(latlngStr2[0]);
-    var lng2 = parseFloat(latlngStr2[1]);
-    var marker2position = new google.maps.LatLng(lat2, lng2);
-    var mk2 = new google.maps.Marker({position: marker2position, map: map});
-
-    /*
-    Utilising the google maps directions API to calculate the distance from
-    marker A to marker B
-    */
-    let directionsService = new google.maps.DirectionsService();
-    let directionsRenderer = new google.maps.DirectionsRenderer();
-
-    //Existing map object displays directions
-    directionsRenderer.setMap(map);
-
-    // Create route from existing points used for markers
-    const route = {
-      origin: marker1position,
-      destination: marker2position,
-      travelMode: 'DRIVING'
-    }
-
-    //Anonymous function to capture directions
-    directionsService.route(route,
-      function(response, status) {
-        if (status !== 'OK') {
-          window.alert('Directions request failed due to ' + status);
-          return;
-        } else {
-          // Add route to the map
-          directionsRenderer.setDirections(response);
-
-          // Get data about the mapped routes
-          var directionsData = response.routes[0].legs[0];
-          if (!directionsData) {
-            window.alert('Directions request failed');
-            return;
-          }else {
-            document.getElementById('msg').innerHTML =
-            "Driving distance and duration from "
-            + marker1position + " to " + marker2position + " is "
-            + directionsData.distance.text + " (" + directionsData.duration.text
-            + ").";
-          }
-        }
-      });
-    });
-
-  //Refresh the map if the 'Refresh' button is clicked
-  document.getElementById("refresh").addEventListener("click", () => {
-    refresh();
+    calcEnteredRoutes();
   });
 
   //Calculating routes based on clicked points
   document.getElementById("calcRoute").addEventListener("click", (clicked) => {
     clicked.stopImmediatePropagation();
     alert("Click on two points on the map to find the driving distance and duration between these two points");
-    google.maps.event.addListener(map, "click", function(event){
-      var clickedPoint1 = new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
-      click1pos = clickedPoint1;
-      var click1marker = new google.maps.Marker({position: clickedPoint1, map: map});
-      document.getElementById("marker1").value = click1pos;
+    calcClickedRoutes();
+  });
 
-      google.maps.event.addListener(map, "click", function(event2){
-        var clickedPoint2 = new google.maps.LatLng(event2.latLng.lat(), event2.latLng.lng());
-        click2pos = clickedPoint2;
-        var click2marker = new google.maps.Marker({position: clickedPoint2, map: map});
-        document.getElementById("marker2").value = click2pos;
-
-        //Calculating distance and duration
-        let directionsService = new google.maps.DirectionsService();
-        let directionsRenderer = new google.maps.DirectionsRenderer();
-        //Existing map object displays directions
-        directionsRenderer.setMap(map);
-
-        // Create route from existing points used for markers
-        const route = {
-           origin: clickedPoint1,
-           destination: clickedPoint2,
-           travelMode: 'DRIVING'
-         }
-
-        //Anonymous function to capture directions
-        directionsService.route(route,
-         function(response, status) {
-           if (status !== 'OK') {
-             window.alert('Directions request failed due to ' + status);
-             return;
-           } else {
-             // Add route to the map
-             directionsRenderer.setDirections(response);
-
-             // Get data about the mapped routes
-             var directionsData = response.routes[0].legs[0];
-             if (!directionsData) {
-               window.alert('Directions request failed');
-               return;
-           } else {
-              document.getElementById('msg').innerHTML =
-              "Driving distance and duration from "
-              + click1pos + " to " + click2pos + " is "
-              + directionsData.distance.text + " (" + directionsData.duration.text
-              + ").";
-            }
-          }
-        });
-      });
-    });
+  //Refresh the map if the 'Refresh' button is clicked
+  document.getElementById("refresh").addEventListener("click", () => {
+    refresh();
   });
 }
